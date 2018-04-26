c0a3a8e2ef9fa2cefa07b040ef10bfe0b9a0843b
Alejandro Espinosa <ra.becerra388@uniandes.edu.co>
2018-04-26 18:17:42 -0500

    ETL completado
    Completado pipeline de ingesta de datos para Cloud Datastore usando Cloud Dataflow.

--- etl/etl.py
+++ etl/etl.py
@@ -2,17 +2,20 @@ from __future__ import absolute_import
 
 import argparse
 import logging
-import re
+from datetime import datetime
 
+# dependencias necesarias para el ETL
 import apache_beam as beam
 from apache_beam.io import ReadFromText
-from apache_beam.io import WriteToText
-from apache_beam.metrics import Metrics
-from apache_beam.metrics.metric import MetricsFilter
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
 
-from google.cloud import datastore
+# dependencias para escritura en base de datos.
+from google.cloud.proto.datastore.v1 import entity_pb2
+from apache_beam.io.gcp.datastore.v1.datastoreio import WriteToDatastore
+
+from googledatastore import helper as datastore_helper
+from googledatastore import PropertyFilter
 
 class csvLoadDoFn(beam.DoFn):
     """Parsea cada linea del csv como una entidad de datastore."""
@@ -21,28 +24,51 @@ class csvLoadDoFn(beam.DoFn):
         """Inicializa el proceso segun los lineamientos de apache beam."""
         super(csvLoadDoFn, self).init()
 
-    def process(self, element):
-        """Procesa cada linea del csv como una entidad de datastore."""
-        pass
-
 class EntityWrapper(object):
     """Crea una entidad de Cloud Datastore dada una string."""
 
-    def __init__(self, namespace, kind):
-        self._namespace = namespace
+    def __init__(self, kind):
+        """Inicializa la entidad con un kind predeterminado."""
         self._kind = kind
 
     def make_entity(self, content):
-        pass
+        """Parsea el contenido del string para crear una entidad de Datastore."""
+        props = content.split(',')
+        entity = entity_pb2.Entity()
+
+        # Crear la llave de la entidad
+        datastore_helper.add_key_path(entity.key, self._kind, props[0])
+
+        birth = datetime(1, 1, 1)
+        try:
+            birth = datetime.strptime(props[4], "%Y-%m-%d")
+        except _:
+            # En caso de no poder formatear correctamente la fecha de nacimiento
+            # se da un valor por defecto.
+            birth = datetime(1, 1, 1)
+        finally:
+            # Se anhaden las propiedades
+            properties = {"name": props[1],
+                          "country": props[2],
+                          "sex": props[3],
+                          "date_of_birth": birth,
+                          "height": props[5],
+                          "weight": props[6],
+                          "gold": props[7],
+                          "silver": props[8],
+                          "bronze": props[9],
+                          "info": props[10]}
+            datastore_helper.add_properties(entity, properties)
+            return entity
 
 def write_to_datastore(user_options, pipeline_options):
     """Crea un pipeline que escribe entidades a Cloud Datastore."""
     with beam.Pipeline(options=pipeline_options) as p:
-        #(p
-        # | 'leer archivo' >>
-        # | 'crear entidad' >>
-        # | 'escribir en Datastore' >>)
-         pass
+        (p
+         | 'leer archivo' >> ReadFromText(user_options.input)
+         | 'crear entidad' >> beam.Map(
+             EntityWrapper(user_options.kind).make_entity)
+         | 'escribir en Datastore' >> WriteToDatastore(user_options.dataset))
 
 def run(argv=None):
     """Punto de acceso principal que corre el pipeline."""
@@ -55,9 +81,15 @@ def run(argv=None):
                         dest='kind',
                         default='Athlete',
                         help='Clase de Datastore')
+    parser.add_argument('--dataset',
+                        dest='dataset',
+                        default='geo-sports',
+                        help='Id del dataset de datastore')
     known_args, pipeline_args = parser.parse_known_args(argv)
 
     # Se obtienen las opciones del pipeline a traves de los argumentos.
+    # Se guarda la sesion principal ya que hay operaciones que dependen del
+    # contexto global
     pipeline_options = PipelineOptions(pipeline_args)
     pipeline_options.view_as(SetupOptions).save_main_session = True
 
@@ -67,3 +99,11 @@ def run(argv=None):
 if __name__=='__main__':
     logging.getLogger().setLevel(logging.INFO)
     run()
+
+# Comando completo para ejecutar el etl. requiere certificados de acceso.
+# python2 etl.py\
+#  --project geo-sports\
+#  --runner DataflowRunner\
+#  --staging_location gs://geo-sports-database-input/staging \
+#  --temp_location gs://geo-sports-database-input/temp \
+#  --output gs://geo-sports-database-input/output
