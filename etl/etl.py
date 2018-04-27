7db03072bca681724aaa208fd9a4206d27fc02e6
Alejandro Espinosa <ra.becerra388@uniandes.edu.co>
2018-04-27 11:50:36 -0500

    Ampliado ETL para generar una vista materializada de los agregados.
    Dadas las limitaciones en query de Datastore se recurre al uso de agregados para simplificar las consultas.

--- etl/etl.py
+++ etl/etl.py
@@ -10,6 +10,10 @@ from apache_beam.io import ReadFromText
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
 
+# Dependencias para metricas
+from apache_beam.metrics import Metrics
+from apache_beam.metrics.metric import MetricsFilter
+
 # dependencias para escritura en base de datos.
 from google.cloud.proto.datastore.v1 import entity_pb2
 from apache_beam.io.gcp.datastore.v1.datastoreio import WriteToDatastore
@@ -17,14 +21,7 @@ from apache_beam.io.gcp.datastore.v1.datastoreio import WriteToDatastore
 from googledatastore import helper as datastore_helper
 from googledatastore import PropertyFilter
 
-class csvLoadDoFn(beam.DoFn):
-    """Parsea cada linea del csv como una entidad de datastore."""
-
-    def __init__(self):
-        """Inicializa el proceso segun los lineamientos de apache beam."""
-        super(csvLoadDoFn, self).init()
-
-class EntityWrapper(object):
+class AthleteEntityWrapper(object):
     """Crea una entidad de Cloud Datastore dada una string."""
 
     def __init__(self, kind):
@@ -39,36 +36,76 @@ class EntityWrapper(object):
         # Crear la llave de la entidad
         datastore_helper.add_key_path(entity.key, self._kind, props[0])
 
-        birth = datetime(1, 1, 1)
-        try:
-            birth = datetime.strptime(props[4], "%Y-%m-%d")
-        except _:
-            # En caso de no poder formatear correctamente la fecha de nacimiento
-            # se da un valor por defecto.
-            birth = datetime(1, 1, 1)
-        finally:
-            # Se anhaden las propiedades
-            properties = {"name": props[1],
-                          "country": props[2],
-                          "sex": props[3],
-                          "date_of_birth": birth,
-                          "height": props[5],
-                          "weight": props[6],
-                          "gold": props[7],
-                          "silver": props[8],
-                          "bronze": props[9],
-                          "info": props[10]}
-            datastore_helper.add_properties(entity, properties)
-            return entity
+        # Se anhaden las propiedades
+        properties = {"name": props[1],
+                      "country": props[2],
+                      "sex": props[3],
+                      "date_of_birth": datetime.strptime(props[4], "%Y-%m-%d"),
+                      "height": props[5],
+                      "weight": props[6],
+                      "gold": props[7],
+                      "silver": props[8],
+                      "bronze": props[9],
+                      "info": props[10]}
+        datastore_helper.add_properties(entity, properties)
+        return entity
+
+class AggregateEntityWrapper(object):
+    """Crea una entidad del agregado en Datastore dada una tupla."""
+
+    def make_entity(self, content):
+        """Con una tupla dada genera una entrada en datastore de los agregados."""
+        entity = entity_pb2.Entity()
+        datastore_helper.add_key_path(entity.key, 'Aggregate')
+
+        properties = {"country": content[0][0],
+                      "sex": content[0][1],
+                      "year": content[0][2],
+                      "total": content[1]}
+        datastore_helper.add_properties(entity, properties)
+        return entity
+
+
+class StripProperties(beam.DoFn):
+    """Parsea cada linea del csv como un diccionario eliminando campos
+    innecesarios para el conteo."""
+
+    def process(self, element):
+        """Retorna un string de cada atleta en el cual solo se tiene en
+        cuenta el pais de origen, el sexo y el anho de nacimiento.
+        Args:
+          element: el elemento de entrada para procesar.
+        Returns:
+          la tupla descrita.
+        """
+        props = element.split(',')
+        dicc = (props[2],
+                props[3],
+                datetime.strptime(props[4], "%Y-%m-%d").year)
+        logging.info(dicc)
+        yield dicc
 
 def write_to_datastore(user_options, pipeline_options):
     """Crea un pipeline que escribe entidades a Cloud Datastore."""
+    def count_athletes(athletes):
+        athletes, ones = athletes;
+        tuple = (athletes, sum(ones))
+        logging.info(tuple)
+        return tuple
+
     with beam.Pipeline(options=pipeline_options) as p:
-        (p
-         | 'leer archivo' >> ReadFromText(user_options.input)
-         | 'crear entidad' >> beam.Map(
-             EntityWrapper(user_options.kind).make_entity)
-         | 'escribir en Datastore' >> WriteToDatastore(user_options.dataset))
+        p0 = (p | 'leer archivo' >> ReadFromText(user_options.input))
+
+        p1 = (p0 | 'Crear entidad de atleta' >> beam.Map(
+                        AthleteEntityWrapper(user_options.kind).make_entity))
+
+        p2 = (p0 | 'Eliminar propiedades' >> beam.ParDo(StripProperties())
+                 | 'Generar agregados' >> beam.combiners.Count.PerElement()
+                 | 'Crear entidad de agregados' >>  beam.Map(
+                        AggregateEntityWrapper().make_entity))
+
+        p3 = ((p1, p2) | 'Unir colecciones' >> beam.Flatten()
+                       | 'escribir en Datastore' >> WriteToDatastore(user_options.dataset))
 
 def run(argv=None):
     """Punto de acceso principal que corre el pipeline."""
